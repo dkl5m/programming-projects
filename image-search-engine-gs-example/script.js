e65b1bf42e4bd03d2efc8e50b96cf802e4413c21
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-01-11 23:56:41 -0300

    Create script.js

---
+++ image-search-engine-gs-example/script.js
@@ -0,0 +1,4 @@
+const searchForm = document.getElementbyId("search-form");
+const searchBox = document.getElementbyId("search-box");
+const searchResult = document.getElementbyId("search-result");
+const showMoreBtn = document.getElementbyId("show-more-btn");
