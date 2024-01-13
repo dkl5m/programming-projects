fd06298408b19a4f2d7c4b8bad48ee9374f7dcde
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-01-12 23:58:02 -0300

    Update script.js adding async function searchImages

--- image-search-engine-gs-example/script.js
+++ image-search-engine-gs-example/script.js
@@ -2,3 +2,11 @@ const searchForm = document.getElementbyId("search-form");
 const searchBox = document.getElementbyId("search-box");
 const searchResult = document.getElementbyId("search-result");
 const showMoreBtn = document.getElementbyId("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+  keyword = searchBox.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+}
