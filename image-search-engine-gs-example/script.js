8d211f7aa59c4f74f93f8efc823f4ae88f44d79a
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-01-13 00:15:12 -0300

    Update script.js with some other results

--- image-search-engine-gs-example/script.js
+++ image-search-engine-gs-example/script.js
@@ -1,3 +1,5 @@
+const accessKey = "";
+
 const searchForm = document.getElementbyId("search-form");
 const searchBox = document.getElementbyId("search-box");
 const searchResult = document.getElementbyId("search-result");
@@ -9,4 +11,20 @@ let page = 1;
 async function searchImages(){
   keyword = searchBox.value;
   const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const results = data.results;
+
+  results.map((results) =>{
+    
+  })
 }
+
+searchForm.addEventListener("submit", (e) =>{
+  e.preventDefault();
+  page = 1;
+  searchImages();
+})
+
