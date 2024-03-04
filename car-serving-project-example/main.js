8600bd0c8081b809877fdb2ff7d2627639b7aac6
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-03-03 23:13:46 -0300

    Update main.js adding some const and attributes

--- car-serving-project-example/main.js
+++ car-serving-project-example/main.js
@@ -4,3 +4,21 @@ const menuBtnIcon = menuBtn.querySelector("i");
 
 menuBtn.addEventListener("click", (e) => {
   navLinks.classList.toggle("open");
+ 
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute(
+    "class",
+    isOpen ? "ri-close-line" : "ri-menu-3-line"
+  );
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-3-line");
+});
+
+const scrollRevealOptions = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
